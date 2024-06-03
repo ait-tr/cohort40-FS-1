cb77c8425a025661a1ab72b283ac50b81d26e5c1
v1148656 <v1148656@gmail.com>
2024-06-03 14:41:08 +0300

    Add lesson_08

---
+++ front_end/lesson_08/code/06_vite_30_05_2024/src/components/UserList.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+
+// rce
+export class UserList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ ...this.state, isLoading: true });
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ users: data, isLoading: false });
+      });
+  }
+
+  render() {
+    const { users, isLoading } = this.state;
+
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>{user.name}</p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default UserList;
---
+++ front_end/lesson_08/plan.md
@@ -0,0 +1,4 @@
+## Задачи на урок:
+
+1. Работа с классовым компонентом;
+2. componentDidMount(), componentDidUpdate(), componentWillUnmount().
