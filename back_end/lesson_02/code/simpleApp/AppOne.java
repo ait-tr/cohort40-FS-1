50fb782fbbae83f79a654f4c451e9c44021e2639
StanislavPB <perminovsb@gmail.com>
2024-06-21 17:25:09 +0100

    lesson 2

---
+++ back_end/lesson_02/code/simpleApp/AppOne.java
@@ -0,0 +1,9 @@
+package code.simpleApp;
+
+public class AppOne {
+    public static void main(String[] args) {
+        ClassOne classOne = new ClassOne();
+        String text = "Text1";
+        classOne.printData(text);
+    }
+}
---
+++ back_end/lesson_02/code/simpleApp/ClassOne.java
@@ -0,0 +1,8 @@
+package code.simpleApp;
+
+public class ClassOne {
+
+    public void printData(String text){
+        System.out.println("Ваш текст: " + text);
+    }
+}
